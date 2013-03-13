0b5ed88289c2a25b1b933af47d5c29e27361c285
Giri Raj <kayalshri@gmail.com>
2013-03-13 11:52:49 +0530

    alignment

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ error_reporting(E_ALL);
 $facebookAppId      = '321176691239227'; 
 $facebookAppSecret  = 'xxxxxxxxxxxxxxxxxxxxxxxxx'; 
 $return_url         = 'http://ngiriraj.com/socialMedia/fbidcard/';  		#Redirect_uri
-$idDir		    	= 'fbcards/'; 											#Dir path to store ID card images
+$idDir		  = 'fbcards/'; 								#Dir path to store ID card images
 $fbPermissions      = 'publish_stream,user_hometown,user_birthday';  		#Required facebook permissions
-$fbTemplate_img     = 'fbidcard.png'; 										#Facebook standared id card image template
-$font               = 'DidactGothic.ttf'; 									#Font used
+$fbTemplate_img     = 'fbidcard.png'; 							#Facebook standared id card image template
+$font               = 'DidactGothic.ttf'; 						#Font used
 
 
 //include facebook SDK
@@ -125,8 +125,8 @@ else
 	//copy($fbProfileImagefile,$fbTemplatefile);
 	save_image($fqlResult[0]['url'],$idDir.$fbuser.'.jpg');    
    
-    //display logout url
-    echo '<div>'.$fb_profile["name"].' [<a href="?logout=true">Log Out</a>]</div>'; 
+    	//display logout url
+    	echo '<div>'.$fb_profile["name"].' [<a href="?logout=true">Log Out</a>]</div>'; 
 
 
 	// Get Friends count and wall count
@@ -139,48 +139,48 @@ else
 	$fqlResult1   =   $facebook->api($param);
 	    
     
-    $fbTemplate = imagecreatefrompng($fbTemplate_img); // FB Template - PNG file format
-    $fbProfileImage = imagecreatefromjpeg($idDir.$fbuser.'.jpg'); // Profile image stored in ID Card folder - jpg file format
+    	$fbTemplate = imagecreatefrompng($fbTemplate_img); // FB Template - PNG file format
+    	$fbProfileImage = imagecreatefromjpeg($idDir.$fbuser.'.jpg'); // Profile image stored in ID Card folder - jpg file format
     
-    imagealphablending($fbTemplate, false); 
-    imagesavealpha($fbTemplate, true);
-    imagecopymerge($fbTemplate, $fbProfileImage, 15, 80, 0, 0, 100, 150, 100);  # profile image merge with FB Template
+    	imagealphablending($fbTemplate, false); 
+    	imagesavealpha($fbTemplate, true);
+    	imagecopymerge($fbTemplate, $fbProfileImage, 15, 80, 0, 0, 100, 150, 100);  # profile image merge with FB Template
     	
 	#Text color Theme
-    $facebook_blue = imagecolorallocate($fbTemplate, 81, 103, 147); // Create blue color
-    $facebook_grey = imagecolorallocate($fbTemplate, 74, 74, 74); // Create grey color
+    	$facebook_blue = imagecolorallocate($fbTemplate, 81, 103, 147); // Create blue color
+    	$facebook_grey = imagecolorallocate($fbTemplate, 74, 74, 74); // Create grey color
     
 	imagealphablending($fbTemplate, true);    
 	
 	#FB User Information
-    $fb_user_id        = $fbuser;
-    $fb_user_name      = isset($fb_profile['name'])?$fb_profile['name']:'No Name';
-    $fb_user_gender    = isset($fb_profile['gender'])?$fb_profile['gender']:'No gender';
-    $fb_user_hometown  = isset($fb_profile['hometown'])?$fb_profile['hometown']['name']:'Unknown';
-    $fb_user_birth     = isset($fb_profile['birthday'])?$fb_profile['birthday']:'00/00/0000';
-    $fb_disclaimer         = '* Un-official facebook ID card from http://ngiriraj.com';
+    	$fb_user_id        = $fbuser;
+    	$fb_user_name      = isset($fb_profile['name'])?$fb_profile['name']:'No Name';
+    	$fb_user_gender    = isset($fb_profile['gender'])?$fb_profile['gender']:'No gender';
+    	$fb_user_hometown  = isset($fb_profile['hometown'])?$fb_profile['hometown']['name']:'Unknown';
+    	$fb_user_birth     = isset($fb_profile['birthday'])?$fb_profile['birthday']:'00/00/0000';
+    	$fb_disclaimer         = '* Un-official facebook ID card from http://ngiriraj.com';
     
-    # Embed informations to FB ID Card	
-    imagettftext($fbTemplate, 12, 0, 130, 107, $facebook_grey, $font, $fb_user_name); // Name
-    imagettftext($fbTemplate, 12, 0, 130, 145, $facebook_grey , $font, $fb_user_id); // ID    
-    imagettftext($fbTemplate, 12, 0, 130, 187, $facebook_grey, $font, $fb_user_gender); //Gender
-    imagettftext($fbTemplate, 12, 0, 273, 187, $facebook_grey, $font, $fb_user_birth); //DOB
-    imagettftext($fbTemplate, 12, 0, 130, 225, $facebook_grey, $font, $fb_user_hometown); //Location
-    imagettftext($fbTemplate, 12, 0, 273, 225, $facebook_grey, $font, $fqlResult1[0]['friend_count']); //Friend count
-    imagettftext($fbTemplate, 12, 0, 365, 225, $facebook_grey, $font, $fqlResult1[0]['wall_count']); //Wall count
-    imagettftext($fbTemplate, 6, 0, 130, 238, $facebook_blue, $font, $fb_disclaimer); //message
+    	# Embed informations to FB ID Card	
+    	imagettftext($fbTemplate, 12, 0, 130, 107, $facebook_grey, $font, $fb_user_name); // Name
+    	imagettftext($fbTemplate, 12, 0, 130, 145, $facebook_grey , $font, $fb_user_id); // ID    
+    	imagettftext($fbTemplate, 12, 0, 130, 187, $facebook_grey, $font, $fb_user_gender); //Gender
+    	imagettftext($fbTemplate, 12, 0, 273, 187, $facebook_grey, $font, $fb_user_birth); //DOB
+    	imagettftext($fbTemplate, 12, 0, 130, 225, $facebook_grey, $font, $fb_user_hometown); //Location
+    	imagettftext($fbTemplate, 12, 0, 273, 225, $facebook_grey, $font, $fqlResult1[0]['friend_count']); //Friend count
+    	imagettftext($fbTemplate, 12, 0, 365, 225, $facebook_grey, $font, $fqlResult1[0]['wall_count']); //Wall count
+    	imagettftext($fbTemplate, 6, 0, 130, 238, $facebook_blue, $font, $fb_disclaimer); //message
         
 	# FB ID Card save into idDir folder	
-    imagepng($fbTemplate, $idDir.'id_'.$fbuser.'.jpg');
+    	imagepng($fbTemplate, $idDir.'id_'.$fbuser.'.jpg');
 	
 	# Display FB ID CARD
-    echo '<img src="'.$idDir.'id_'.$fbuser.'.jpg" >';
+    	echo '<img src="'.$idDir.'id_'.$fbuser.'.jpg" >';
     
 	# DELETE Profile image (due to space issue)
-    unlink($idDir.$fbuser.'.jpg');    
+    	unlink($idDir.$fbuser.'.jpg');    
     
 	# Error : Uncaught OAuthException: (#200) The user hasn't authorized the application to perform this action thrown in ...
-    # [REF] http://stackoverflow.com/questions/9996859/oauthexception-when-posting-to-facebook-wall-using-php/10651503#10651503
+    	# [REF] http://stackoverflow.com/questions/9996859/oauthexception-when-posting-to-facebook-wall-using-php/10651503#10651503
 	$facebook->setFileUploadSupport(true);
 	
 	
@@ -188,7 +188,7 @@ else
 	$post_data = array(
 		'name'=>"ngiriraj",
 		'description'=>"ID Card Maker"
-    );
+    	);
 	$data['album'] = $facebook->api("/me/albums", 'post', $post_data); // You will get Album ID
 	$album_id = $data['album']['id'];
 	
@@ -202,7 +202,7 @@ else
 	);
 	$data['photo'] = $facebook->api("/$album_id/photos", 'post', $post_data);  
 	
-    imagedestroy($fbTemplate);
-    imagedestroy($fbProfileImage);
+    	imagedestroy($fbTemplate);
+    	imagedestroy($fbProfileImage);
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -8,10 +8,10 @@ Facebook ID card maker:
 /* CONFIGURE */
 $facebookAppId      = '321176691239227'; 
 $facebookAppSecret  = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; 
-$return_url         = 'http://ngiriraj.com/socialMedia/fbidcard/';  		#Redirect_uri
-$idDir		    	= 'fbcards/'; 											#Dir path to store ID card images
-$fbPermissions      = 'publish_stream,user_hometown,user_birthday';  		#Required facebook permissions
-$fbTemplate_img     = 'fbidcard.png'; 										#Facebook standared id card image template
-$font               = 'DidactGothic.ttf'; 									#Font used
+$return_url         = 'http://ngiriraj.com/socialMedia/fbidcard/'; #Redirect_uri
+$idDir		    = 'fbcards/'; #Dir path to store ID card images
+$fbPermissions      = 'publish_stream,user_hometown,user_birthday';#Required facebook permissions
+$fbTemplate_img     = 'fbidcard.png'; #Facebook standared id card image template
+$font               = 'DidactGothic.ttf'; #Font used
 
 
